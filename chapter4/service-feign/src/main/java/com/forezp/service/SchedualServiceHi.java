1527358b667993da5c5f2d0a481ca522e3d1b2bb
Forezp <forezp@Pc-20130412.local>
2017-04-09 21:17:03 +0800

    hystrix

---
+++ chapter4/service-feign/src/main/java/com/forezp/service/SchedualServiceHi.java
@@ -0,0 +1,15 @@
+package com.forezp.service;
+
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Created by fangzhipeng on 2017/4/6.
+ */
+@FeignClient(value = "service-hi",fallback = SchedualServiceHiHystric.class)
+public interface SchedualServiceHi {
+    @RequestMapping(value = "/hi",method = RequestMethod.GET)
+    String sayHiFromClientOne(@RequestParam(value = "name") String name);
+}

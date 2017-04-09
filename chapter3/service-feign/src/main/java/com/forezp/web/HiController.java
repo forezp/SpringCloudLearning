a422d4b7e63052ac4af50671756a0263237a4a7c
Forezp <forezp@Pc-20130412.local>
2017-04-09 11:56:07 +0800

    feign

---
+++ chapter3/service-feign/src/main/java/com/forezp/web/HiController.java
@@ -0,0 +1,22 @@
+package com.forezp.web;
+
+import com.forezp.service.SchedualServiceHi;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by fangzhipeng on 2017/4/6.
+ */
+@RestController
+public class HiController {
+
+    @Autowired
+    SchedualServiceHi schedualServiceHi;
+    @RequestMapping(value = "/hi",method = RequestMethod.GET)
+    public String sayHi(@RequestParam String name){
+        return schedualServiceHi.sayHiFromClientOne(name);
+    }
+}

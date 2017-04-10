aefd6ea70b9232e78a2987015c0851c315e9da7f
Forezp <forezp@Pc-20130412.local>
2017-04-10 21:39:27 +0800

    config

---
+++ chapter7/config-server/src/main/java/com/forezp/ConfigServerApplication.java
@@ -0,0 +1,17 @@
+package com.forezp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+
+@SpringBootApplication
+@EnableConfigServer
+@EnableEurekaClient
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+}

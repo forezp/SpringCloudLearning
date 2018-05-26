4823ed84f82cdd581b0af9c6f9d14efb268456c3
baidu <liangfei@baidu.com>
2018-05-26 14:33:46 +0800

    update micro service for ha demo

---
+++ chapter15/service-ribbon/src/main/java/com/forezp/service/HelloService.java
@@ -0,0 +1,26 @@
+package com.forezp.service;
+
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * Created by fangzhipeng on 2017/4/6.
+ */
+@Service
+public class HelloService {
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    @HystrixCommand(fallbackMethod = "hiError")
+    public String hiService(String name) {
+        return restTemplate.getForObject("http://SERVICE-HI/hi?name="+name,String.class);
+    }
+
+    public String hiError(String name) {
+        return "hi,"+name+",sorry,error!";
+    }
+}

1ba57bbadcee9f9e3af5ae155e530185c1372da3
fangzhipeng <124746406@qq.com>
2018-11-02 18:04:34 +0800

    gateway

--- sc-f-gateway-first-sight/src/main/java/gateway/Application.java
+++ sc-f-gateway-first-sight/src/main/java/gateway/Application.java
@@ -4,8 +4,6 @@ import reactor.core.publisher.Mono;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;

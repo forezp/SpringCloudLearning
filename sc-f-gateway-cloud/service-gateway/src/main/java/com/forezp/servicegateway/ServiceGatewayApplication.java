0f1ba1e2cfc83f67461185e48a7fa55acbf5c528
fangzhipeng <124746406@qq.com>
2018-11-16 22:59:33 +0800

    gateway cloud

---
+++ sc-f-gateway-cloud/service-gateway/src/main/java/com/forezp/servicegateway/ServiceGatewayApplication.java
@@ -0,0 +1,12 @@
+package com.forezp.servicegateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run( ServiceGatewayApplication.class, args );
+    }
+}

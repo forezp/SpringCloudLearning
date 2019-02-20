8fdbdc7780e55b537eee366418fbd2aee068a96f
fangzhipeng <fangzhipeng@xiaoantimes.com>
2019-02-20 21:52:09 +0800

    config-jdbc

---
+++ chapter10-5-jdbc/config-server/src/main/java/com/forezp/ConfigServerApplication.java
@@ -0,0 +1,14 @@
+package com.forezp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+}
